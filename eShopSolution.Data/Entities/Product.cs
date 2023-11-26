9c25e5b0812312240c445fa316c51784fb69d1a1
LaDangHuy <lahuy136@gmail.com>
2023-11-26 22:33:54 +0700

    Config entities using Fluent API

---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Price { set; get; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public int ViewCount { set; get; }
+        public DateTime DateCreated { set; get; }
+        public bool? IsFeatured { get; set; }
+
+        //public List<ProductInCategory> ProductInCategories { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<ProductTranslation> ProductTranslations { get; set; }
+        //public List<ProductImage> ProductImages { get; set; }
+    }
+}
